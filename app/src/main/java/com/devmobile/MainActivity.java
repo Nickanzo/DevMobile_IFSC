3a202c77d378726be11a5debb1111870aa65bfcc
Nicolas <nicoeanzola@gmail.com>
2025-03-27 20:51:07 -0300

    criando Botao e TextView

--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.devmobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt = findViewById(R.id.text);
+    Button bt = findViewById(R.id.button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
