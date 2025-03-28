62fd8f74df34ad5d5864f5ddcc62003cbf9a88b1
Nicolas <nicoeanzola@gmail.com>
2025-03-27 22:00:42 -0300

    adicionando validacoes de campos ao interagir com a interface

--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -1,6 +1,12 @@
 package com.devmobile;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +15,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+// Variaveis para Sorteio de numero entre intervalo
+    int min, max, res=0;
+    protected boolean validaEntrada(String str1 , String str2){
+        return !TextUtils.isEmpty(str1) && !TextUtils.isEmpty(str2);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setTitle("Sorteio");
         setContentView(R.layout.activity_main);
+    // Objetos da Atividade para manipulacao
+        EditText inicio = findViewById(R.id.inicio);
+        EditText fim = findViewById(R.id.fim);
+        TextView txt = findViewById(R.id.resultado);
+        Button bt = findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            // Valida dados inseridos
+               if (validaEntrada(inicio.getText().toString().trim(),
+                        fim.getText().toString().trim())){
+               // Busca os valores inseridos na tela
+                   min = Integer.parseInt(inicio.toString());
+                   max = Integer.parseInt(fim.toString());
+               // Verifica se o intervalo esta correto
+                   if (min > max){
+                       txt.setText("Inicio do intervalo maior ao fim do intervalo");
+                       txt.setTextColor(Color.parseColor("#FF00000"));
+                   }else{
+
+                   }
+               }
+            }
+        });
     }
 }
