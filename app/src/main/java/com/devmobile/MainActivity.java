9f2f8acccd60d775dfcd3a00a4e769abb4b7ba46
Nickanzo <nicoeanzola@gmail.com>
2026-03-03 22:21:40 -0300

    Polindo contador de Click

--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -18,15 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);    
         TextView txt = findViewById(R.id.text);
         Button bt = findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        txt.setText(R.string.Hello);
+        bt.setOnClickListener( view -> {
                 count++;
                 txt.setText(String.format("%d",count));
-            }
         });
     }
 }
