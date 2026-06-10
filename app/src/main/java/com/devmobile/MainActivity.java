bd21f8a23bbaca0da2e6d289f0564f49b1259983
nickanzo <nicoeanzola@gmail.com>
2026-06-10 19:31:16 -0300

    Testando Persistência de dados com SharedPreferences

--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.devmobile;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+    SharedPreferences sp;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tv=findViewById(R.id.textViewTitle);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), ((v, insets) -> {
+
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        }));
+
+        sp = getSharedPreferences("preferencias", MODE_PRIVATE);
+
+        sp.edit().putString("nome", "joao");
+
+        tv.setText(sp.getString("nome", "".toString()));
+
+
     }
 }
