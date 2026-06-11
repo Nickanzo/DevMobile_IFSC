b04abe65f88e5eff7cdac7027dec0403711eaef0
nickanzo <nicoeanzola@gmail.com>
2026-06-10 21:51:31 -0300

    Adiciona botão e EditText para incluir na lista/Banco

--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -22,42 +24,50 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase sqldb;
     ContentValues cv;
     ListView listView;
+    EditText editText;
+    Button button;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listaQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
         listView = findViewById(R.id.list);
+        button = findViewById(R.id.button);
 
         sqldb=openOrCreateDatabase("banco.db", MODE_PRIVATE, null);
 
         sqldb.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT , titulo TEXT, conteudo TEXT)");
 
-        cv = new ContentValues();
-        cv.put("titulo", "Nota 1");
-        cv.put("conteudo", "Conteudo da nota 1");
+        button.setOnClickListener(view -> {
+            String str = editText.getText().toString();
+            cv = new ContentValues();
+            cv.put("titulo", "X");
+            cv.put("conteudo", str);
+            sqldb.insert("notas", null, cv );
+            listarDB();
+        });
+        listarDB();
+    }
 
-        sqldb.insert("notas", null, cv );
-
-
+    public void listarDB(){
         Cursor cursor = sqldb.rawQuery("SELECT * FROM notas", null);
         cursor.moveToFirst();
-
-        ArrayList<String> listaQuery = new ArrayList<>();
+        listaQuery = new ArrayList<>();
         while(!cursor.isAfterLast()){
 
-            @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex("id"));
-            @SuppressLint("Range") String titulo = cursor.getString(cursor.getColumnIndex("titulo"));
-            @SuppressLint("Range") String conteudo = cursor.getString(cursor.getColumnIndex("conteudo"));
+            int id = cursor.getInt(cursor.getColumnIndex("id"));
+            String titulo = cursor.getString(cursor.getColumnIndex("titulo"));
+            String conteudo = cursor.getString(cursor.getColumnIndex("conteudo"));
             listaQuery.add("id: " + Integer.toString(id) + " titulo "+  titulo + " conteudo: " + conteudo);
-
             cursor.moveToNext();
             Log.d("SELECT notas" , "id: " + id + " titulo: " + titulo + " conteudo: " + conteudo);
 
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaQuery);
+            listView.setAdapter(adapter);
         };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaQuery);
-        listView.setAdapter(adapter);
     }
 }
