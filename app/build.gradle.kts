6126a1c87aef6827b2bafe3ce3ccb17278246541
Nickanzo <nicoeanzola@gmail.com>
2026-06-17 21:31:25 -0300

    Criação de Projeto com PreviewView do CameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,14 +29,27 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-video:1.3.4")
+
+    implementation("androidx.camera:camera-view:1.3.4")
+    implementation("androidx.camera:camera-extensions:1.3.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/devmobile/MainActivity.java
+++ app/src/main/java/com/devmobile/MainActivity.java
@@ -1,19 +1,126 @@
 package com.devmobile;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.health.connect.datatypes.Record;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.FallbackStrategy;
+import androidx.camera.video.MediaStoreOutputOptions;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import com.devmobile.databinding.ActivityMainBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    public interface LumaListener {
+        void onLumaCalculated(double luma);
+    }
+
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    PreviewView previewView;
+
+    private ActivityMainBinding viewBinding;
+
+    private ImageCapture imageCapture = null;
+    private VideoCapture<Recorder> videoCapture = null;
+    private Recording recording = null;
+
+    private ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        previewView=findViewById(R.id.viewFinder);
+
+        if (checkAndRequestPermission()){
+            startCamera();
+        }else{
+            Toast.makeText(getBaseContext(), "Sem permissao", Toast.LENGTH_LONG).show();
+        }
+
+        //findViewById(R.id.image_capture_button).setOnClickListener(v -> takePhoto());
+        findViewById(R.id.video_capture_button).setOnClickListener(v -> toggleVideo());
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraExecutor != null) {
+            cameraExecutor.shutdown();
+        }
+    }
+
+    public boolean checkAndRequestPermission(){
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+
+        return true;
+    }
+
+    private void startCamera(){
+
+        ProcessCameraProvider.getInstance(this).addListener(() -> {
+            try {
+                ProcessCameraProvider provider = ProcessCameraProvider.getInstance(this).get();
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                imageCapture = new ImageCapture.Builder().build();
+                Recorder recorder = new Recorder.Builder()
+                        .setQualitySelector(QualitySelector.from(Quality.HD))
+                        .build();
+                videoCapture = VideoCapture.withOutput(recorder);
+                provider.unbindAll();
+                provider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA,
+                        preview, imageCapture, videoCapture);
+            } catch (Exception e) { e.printStackTrace(); }
+        }, ContextCompat.getMainExecutor(this));
+
+    }
+
+    private void toggleVideo(){
+
+
+    }
+
+
 }
